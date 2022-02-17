c4f53edc3b400aa720437908af484439fc49240a
ls30378 <ls30378@ubt-uni.net>
2022-02-17 16:37:06 +0100

    Fixed yarn, added routes, model to express

--- comment/server.js
+++ comment/server.js
@@ -1,6 +1,22 @@
+require('dotenv').config()
+const cors = require('cors')
 const express = require('express')
 
 const app = express()
 
-app.listen(4000, () => console.log('Server Started'))
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL)
+console.log('started')
+
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to database'))
+
+app.use(express.json())
+
+const commentRouter = require('./routes/comment')
+app.use('/comment', commentRouter)
+
+app.listen(4000, '0.0.0.0', () => console.log('Server Started'))
 
